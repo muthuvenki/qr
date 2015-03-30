8c2dc8cd7e5b9b2df3bd9971ef0eca4f1b3bfe4a
muthu venki <venkatmuthiahc@gmail.com>
2015-03-31 02:07:54 +0530

    final
    completed

--- home.php
+++ home.php
@@ -18,8 +18,8 @@
 include("phpqrcode/qrlib.php");
 include("qrchecker.php");
 
-ini_set('max_execution_time', 1000); //300 seconds = 5 minutes
-$start=substr($_POST['start'], -4);//to get starting k! id
+ini_set('max_execution_time', 1000); 
+$start=substr($_POST['star'], -4);//to get starting k! id
 $end=substr($_POST['end'], -4);
 
 $convert = sprintf("%04d", $start);//to satisfy the condition of 4 digit number (xxxx)
@@ -34,17 +34,18 @@ $current="KQ"."$date"."$convert";
 for ($i = $start; $i <= $end; $i++)
 {
 	if($pre == $current){
-        $pre=$convert+1;
-        echo $pre;
+        $pre=$convert;
+        //echo $convert;
+        //echo $pre+0001;
 	echo '</tr><tr>';
  	for($j=1;$j<=10;$j++)
 	{
 		$k="KQ"."$date"."$convert";
-		QRcode::png($k,$k.'.png', QR_ECLEVEL_L);
-        $result=qrchecker($k.'.png');
+		QRcode::png($k,$k.'.png', QR_ECLEVEL_L);//to call a  qrcode lib to print qr code
+        $result=qrchecker($k.'.png');//to call an api to read a qr code
         if($k == $result){
         $current=substr($result, -4);
-        echo $current;
+        //echo $current;
         echo '<td><p align=center><img  src="'.$k.'.png" height="75px"/></p><p class="qrcontent">'.$k.'</p></td>';
         //echo $k;
         }
--- index.php
+++ index.php
@@ -2,10 +2,27 @@
 <html>
 <head>
 	<title></title>
+	<script type="text/javascript">
+function check()
+	{
+		var start=document.get.star.value);
+		var end=document.get.end.value;
+		if (end-start <= 16)
+				       return true;
+			else{
+				alert('pls readt the NOTE carefully');
+				document.get.star.value="";
+				document.get.star.focus();
+				return false;
+		}
+			
+	}
+	</script>
 </head>
 <body>
-<form action="home.php" method="post">
-<input type="text" name="start">
+	<p>NOTE : THE K! id must be start with KQ15---- to KQ15---- and number range must be 16 exactly
+<form action="home.php" method="post" name="get" onSubmit="return check()">
+<input type="text" name="star">
 <input type="text" name="end">
 <input type="submit" value="generate">
 </form>
